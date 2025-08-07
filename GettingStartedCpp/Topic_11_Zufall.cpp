30870f242892950ea19a43a6c706dec1921f7163
Peter Loos <cpp@peterloos.de>
2025-08-07 11:50:48 +0200

    11.50

--- GettingStartedCpp/Topic_11_Zufall.cpp
+++ GettingStartedCpp/Topic_11_Zufall.cpp
@@ -6,12 +6,17 @@ void zufall_00()
     std::println("zufall:");
 
     int n1 = std::rand();
+
+    // 0 .. 48 plus 1
+    n1 = (n1 % 49) + 1;
     std::println("Zahl: {}", n1);
 
     int n2 = std::rand();
+    n2 = (n2 % 49) + 1;
     std::println("Zahl: {}", n2);
 
     int n3 = std::rand();
+    n3 = (n3 % 49) + 1;
     std::println("Zahl: {}", n3);
 }
 
